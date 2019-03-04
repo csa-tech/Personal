0909fc253daebf466964357d2c1a179f661f3598
Jim Wan <jimwan@169-231-141-190.wireless.ucsb.edu>
2019-03-04 10:07:48 -0800

    new Personal

--- controllers/pageviewCtrl.js
+++ controllers/pageviewCtrl.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   user     : 'CSAUser',
   password : 'Csa666!!',
   port     : '3306',
-  database : 'myDataBase'
+  database : 'rideshare'
 });
 
 connection.connect(function(err) {
@@ -16,87 +16,118 @@ connection.connect(function(err) {
   console.log('Connected to database.');
 });
 
-// 从secret.json文件里读取数据并转换为Object
-// var secret = JSON.parse(require('fs').readFileSync('secret.json', 'utf8'));
-
-// var connection = mysql.createConnection(secret);
-// connection.connect();
-
-
-
-Getpersonal = function (req, res, next) {
+Getpersonal = function (req, res, next) { // only get
   try {
     var input = req.query;
-    connection.query(`SELECT * FROM rideshare.personal WHERE user_ID = '${input.user_ID}';`, function(err, rows, fields) { //这里写SQL query
-      if (err) { throw err; } //出错时交给后面的finally去处理      
-
-      // 成功时的处理
-      // rows = [ RowDataPacket { count: 0, update_time: 2019-01-15T23:34:59.000Z } ]
+    connection.query(`SELECT * FROM rideshare.user_info WHERE user_id = '${input.user_ID}';`, function(err, rows, fields) { //这里写SQL query
+      if (err) { throw err; } 
       res.status(200).send(rows);
     });
-  } catch(err) { // 这里使用try...catch语法，即使出bug也可以正常应答，并且断开和数据库的连接
-    res.status(500).send('SERVER ERROR:' + err); // SQL出错时的处理
+  } catch(err) {
+    res.status(500).send('SERVER ERROR:' + err);
+    connection.end();
+  }
+}
+                          // can't set header after they are sent what is htis man
+InputPersonal = function(req, res, next){
+  // req = request
+  // res = response
+  try{
+    var input = req.query;
+    var string = input.user_ID;
+    connection.query(`SELECT * FROM rideshare.user_info WHERE user_id = '${string}';`, function(err, rows, fields) {
+      if (err) {throw err;}
+      name = rows[0].name;
+      connection.query(`UPDATE rideshare.user_info SET name = '` + name + `' WHERE user_ID = '${string}';`, function(err, rows, fields) {
+        if(err){throw err;}
+        res.status(200).send('Success. New Name: ' + name);
+      });
+      phoneNum = rows[0].phoneNum;
+      connection.query(`UPDATE rideshare.user_info SET phoneNum = '` + phoneNum + `' WHERE user_ID = '${string}';`, function(err, rows, fields) {
+        if(err){throw err;}
+        res.status(200).send('Success. New phoneNum: ' + phoneNum);
+      });
+      carType = rows[0].carType;
+      connection.query(`UPDATE rideshare.user_info SET carType = '` + carType + `' WHERE user_ID = '${string}';`, function(err, rows, fields) {
+        if(err){throw err;}
+        res.status(200).send('Success. New carType: ' + carType);
+      });
+      carLicense = rows[0].carLicense;
+      connection.query(`UPDATE rideshare.user_info SET carLicense = '` + carLicense + `' WHERE user_ID = '${string}';`, function(err, rows, fields) {
+        if(err){throw err;}
+        res.status(200).send('Success. New carLicense: ' + carLicense);
+      });
+      carColor = rows[0].carColor;
+      connection.query(`UPDATE rideshare.user_info SET carColor = '` + carColor + `' WHERE user_ID = '${string}';`, function(err, rows, fields) {
+        if(err){throw err;}
+        res.status(200).send('Success. New carColor: ' + carColor);
+      });
+    });
+  }catch(err){
+    res.status(500).send("Server Error: " + err);
     connection.end();
   }
 }
 
+// InputPersonal = function(req, res, next) {
+//   try {
+//     var input = req.query;
+//     var string = input.user_ID
+//     // queryString = 'SELECT count FROM rideshare.pageview;'
+//     console.log(`SELECT * FROM rideshare.user_info WHERE user_id = '${string}';`)
+//     connection.query(`SELECT * FROM rideshare.user_info WHERE user_id = '${string}';`, function(err, rows, fields) {
+//       // input.user_ID appears to be undefined
+//       if (err) { throw err; } 
+//       //图片不知道怎么上传
+      
+//       console.log(rows[0]); // her comes out to be undefined maybe try rows.nickname
+      
+//       nickName = rows[0];
 
-InputPersonal = function(req, res, next) {
-  try {
-    var input = req.query;
-    var string = input.user_ID
-    // queryString = 'SELECT count FROM rideshare.pageview;'
-    connection.query(`SELECT * FROM rideshare.personal WHERE user_ID = '${input.user_ID}';`, function(err, rows, fields) {
-      // input.user_ID appears to be undefined
-      if (err) { throw err; }
-      //图片不知道怎么上传
+//       queryString = `UPDATE rideshare.user_info SET name=` + nickName +` WHERE user_ID = '${string}'`
+//       connection.query(queryString, function(err, rows, fields) {
+//         if (err) { throw err; }
+//         res.status(200).send('Sucess. New nickName: ' + nickName); 
+//       });
 
-      nickName = rows[1].nickName;
+//       phoneNum = rows[0]
 
-      queryString = `UPDATE rideshare.personal SET nickName= + nickName + WHERE user_ID = '${string}', update_time=NOW();`
-      connection.query(queryString, function(err, rows, fields) {
-        if (err) { throw err; }
-        res.status(200).send('Sucess. New nickName: ' + nickName); 
-      });
+//       queryString = `UPDATE rideshare.user_info SET phoneNum=` + phoneNum +` WHERE user_ID = '${string}'`
+//       connection.query(queryString, function(err, rows, fields) {
+//         if (err) { throw err; }
+//         res.status(200).send('Sucess. New phoneNum: ' + phoneNum); 
+//       });
 
-      phoneNum = rows[2].phoneNum;
+//       carType = rows[0].carType;
 
-      queryString = `UPDATE rideshare.personal SET phoneNum= + phoneNum + WHERE user_ID = '${string}', update_time=NOW();`
-      connection.query(queryString, function(err, rows, fields) {
-        if (err) { throw err; }
-        res.status(200).send('Sucess. New phoneNum: ' + phoneNum); 
-      });
+//       queryString = `UPDATE rideshare.user_info SET carType= `+ carType +` WHERE user_ID = '${string}'`
+//       connection.query(queryString, function(err, rows, fields) {
+//         if (err) { throw err; }
+//         res.status(200).send('Sucess. New carType: ' + carType); 
+//       });
 
-      carType = rows[3].carType;
+//       carNum = rows[0].carLicense;
 
-      queryString = `UPDATE rideshare.personal SET carType=' + carType + 'WHERE user_ID = '${string}', update_time=NOW();`
-      connection.query(queryString, function(err, rows, fields) {
-        if (err) { throw err; }
-        res.status(200).send('Sucess. New carType: ' + carType); 
-      });
+//       queryString = `UPDATE rideshare.user_info SET carLicense=`+ carNum +` WHERE user_ID = '${string}'`
+//       connection.query(queryString, function(err, rows, fields) {
+//         if (err) { throw err; }
+//         res.status(200).send('Sucess. New carLicense: ' + carNum); 
+//       });
 
-      carNum = rows[4].carNum;
+//       carCol = rows[0].carColor;
 
-      queryString = `UPDATE rideshare.personal SET carNum=' + carNum + 'WHERE user_ID = '${string}', update_time=NOW();`
-      connection.query(queryString, function(err, rows, fields) {
-        if (err) { throw err; }
-        res.status(200).send('Sucess. New carNum: ' + carNum); 
-      });
+//       queryString = `UPDATE rideshare.user_info SET carColor= `+ carCol +` WHERE user_ID = '${string}'`
+//       connection.query(queryString, function(err, rows, fields) {
+//         if (err) { throw err; }
+//         res.status(200).send('Sucess. New carColor: ' + carCol); 
+//       });
 
-      carCol = rows[5].carCol;
-
-      queryString = `UPDATE rideshare.personal SET carCol=' + carCol + 'WHERE user_ID = '${string}', update_time=NOW();`
-      connection.query(queryString, function(err, rows, fields) {
-        if (err) { throw err; }
-        res.status(200).send('Sucess. New carCol: ' + carCol); 
-      });
-
-    });
-  } catch(err) { 
-    res.status(500).send('SERVER ERROR: ' + err); 
-    connection.end();
-  } 
-}
+//     });
+//   } catch(err) { 
+//     res.status(500).send('SERVER ERROR: ' + err); 
+//     connection.end();
+//   } 
+// }
 
   // 前面我们定义了view, add两个函数，现在我们要将它们导出，以供routes使用
 
